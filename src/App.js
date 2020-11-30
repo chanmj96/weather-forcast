08798d64653f20407beb53f847ab8757c3137467
Matthew Chan <matthew.chan@liferay.com>
2020-11-29 23:33:00 -0800

    Init repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="weather-app">
+      <h2 className="title">Weather App</h2>
+
+    </div>
+  );
+}
+
+export default App;
