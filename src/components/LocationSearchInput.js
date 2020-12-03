d6e129daa4bb98d33bd9ada7ff60f639e80007f1
Matthew Chan <matthew.chan@liferay.com>
2020-12-03 14:12:09 -0800

    Update to utilize sass styles

--- src/components/LocationSearchInput.js
+++ src/components/LocationSearchInput.js
@@ -14,32 +14,34 @@ function LocationSearchInput(props) {
 	};
 
 	return (
-		<PlacesAutocomplete
-			value={props.address}
-			onChange={(address) => props.updateAddress(address)}
-			onSelect={handleSelect}
-		>
-			{({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-				<div>
-					<input
-						{...getInputProps({
-							placeholder: 'Search Places ...',
-							className: 'location-search-input',
-						})}
-					/>
-					<div className={`autocomplete-dropdown-container ${suggestions.length > 0 ? 'd-block' : 'd-none' }`}>
-						{loading && <div>Loading...</div>}
-						{suggestions.map((suggestion, i) => {
-							return (
-								<div key={i} className={suggestion.active ? 'suggestion-item--hover' : 'suggestion-item'} {...getSuggestionItemProps(suggestion)}>
-									<span>{suggestion.description}</span>
-								</div>
-							);
-						})}
+		<div className="location-search-container">
+			<PlacesAutocomplete
+				value={props.address}
+				onChange={(address) => props.updateAddress(address)}
+				onSelect={handleSelect}
+			>
+				{({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+					<div>
+						<input
+							{...getInputProps({
+								placeholder: 'Search Places ...',
+								className: 'location-search-input',
+							})}
+						/>
+						<div className={`autocomplete-dropdown-container ${suggestions.length > 0 ? 'd-block' : 'd-none' }`}>
+							{loading && <div>Loading...</div>}
+							{suggestions.map((suggestion, i) => {
+								return (
+									<div key={i} className={suggestion.active ? 'suggestion-item--hover' : 'suggestion-item'} {...getSuggestionItemProps(suggestion)}>
+										<span>{suggestion.description}</span>
+									</div>
+								);
+							})}
+						</div>
 					</div>
-				</div>
-			)}
-		</PlacesAutocomplete>
+				)}
+			</PlacesAutocomplete>
+		</div>
 	);
 }
 
