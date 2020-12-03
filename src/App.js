e4f2098810c0857e076fdc784d404150d0952e97
Matthew Chan <matthew.chan@liferay.com>
2020-12-02 16:04:57 -0800

    Add weather form elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import LocationSearchInput from './components/LocationSearchInput';
 
 function App() {
   return (
     <div className="weather-app">
       <h2 className="title">Weather App</h2>
 
+      <button className="current-location-button">
+        Current Location
+      </button>
+
+      <div className="location-search-input">
+        <LocationSearchInput />
+      </div>
     </div>
   );
 }
