b97111117e3a0daf13681efb4b8fce9260b3d3c0
Matthew Chan <matthew.chan@liferay.com>
2020-12-06 16:03:33 -0800

    Remove Map default controls on Mobile devices

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,33 +1,73 @@
 import React from 'react';
+import Media from 'react-media';
 import { Map, Marker } from 'google-maps-react';
 
 function MapContainer (props) {
 
 	return (
-		<Map
-			google={window.google}
-			zoom={14}
-			className="map-display"
-			center={
-				{
-					lat: props.mapLatitude,
-					lng: props.mapLongitude
-				}
-			}
-			initialCenter={
-				{
-					lat: props.locationDefault.lat,
-					lng: props.locationDefault.lng
-				}
-			}
-		>
-			{props.showMarker &&
-				<Marker
-					title={props.address}
-					position={{lat: props.mapLatitude, lng: props.mapLongitude}}>
-				</Marker>
-			}
-		</Map>
+		<Media queries={{
+			small: "(max-width: 599px)",
+			medium: "(min-width: 600px) and (max-width: 1199px)",
+			large: "(min-width: 1200px)"
+		}}>
+			{matches => (
+				<React.Fragment>
+					{matches.small &&
+						<Map
+							google={window.google}
+							zoom={14}
+							className="map-display"
+							center={
+								{
+									lat: props.mapLatitude,
+									lng: props.mapLongitude
+								}
+							}
+							disableDefaultUI="true"
+							initialCenter={
+								{
+									lat: props.locationDefault.lat,
+									lng: props.locationDefault.lng
+								}
+							}
+						>
+							{props.showMarker &&
+							<Marker
+								title={props.address}
+								position={{lat: props.mapLatitude, lng: props.mapLongitude}}>
+							</Marker>
+							}
+						</Map>
+					}
+					{(matches.medium || matches.large) &&
+						<Map
+							google={window.google}
+							zoom={14}
+							className="map-display"
+							center={
+								{
+									lat: props.mapLatitude,
+									lng: props.mapLongitude
+								}
+							}
+							initialCenter={
+								{
+									lat: props.locationDefault.lat,
+									lng: props.locationDefault.lng
+								}
+							}
+						>
+							{props.showMarker &&
+							<Marker
+								title={props.address}
+								position={{lat: props.mapLatitude, lng: props.mapLongitude}}>
+							</Marker>
+							}
+						</Map>
+					}
+				</React.Fragment>
+			)}
+		</Media>
 	);
 }
 
